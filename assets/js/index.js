b0efaeae9aba8ae56ea5d0b543d83f2572e61726
codeBiscuit <josh.rinehart.2k@gmail.com>
2023-05-11 23:50:14 -0400

    added nav functionality

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+const navEl = document.querySelector('ol');
+const homeEl = document.getElementById('home');
+const projectsEl = document.getElementById('projects');
+const contactEl = document.getElementById('contact');
+
+const pages = [homeEl, projectsEl, contactEl];
+
+function pageDeselect(item){
+    if (item.getAttribute('class') === 'page selected'){
+        item.setAttribute('class', 'page');
+        item.setAttribute('style', 'display: none');
+    }
+}
+
+function pageSelect(event){
+    event.preventDefault();
+
+    const newLocation = event.target.textContent.toLowerCase();
+
+    pages.forEach(pageDeselect);
+
+    if (newLocation === 'home'){
+        homeEl.setAttribute('class', 'page selected');
+        homeEl.setAttribute('style', 'display: block');
+    }
+    else if (newLocation === 'projects'){
+        projectsEl.setAttribute('class', 'page selected');
+        projectsEl.setAttribute('style', 'display: block');
+    }
+    else if (newLocation === 'contact'){
+        contactEl.setAttribute('class', 'page selected');
+        contactEl.setAttribute('style', 'display: block');
+    }
+}
+
+navEl.addEventListener('click', pageSelect)
