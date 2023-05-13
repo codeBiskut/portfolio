2f4ba8e4fc579a0988278a454b6ad22ad101ef36
codeBiscuit <josh.rinehart.2k@gmail.com>
2023-05-12 20:58:09 -0400

    nav item now stays half opaque when selected

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,39 +2,55 @@ const navEl = document.querySelector('ol');
 const homeEl = document.getElementById('home');
 const projectsEl = document.getElementById('projects');
 const contactEl = document.getElementById('contact');
+const navHomeEl = document.getElementById('nav-home');
+const navProjectsEl = document.getElementById('nav-projects');
+const navContactEl = document.getElementById('nav-contact');
 
 const pages = [homeEl, projectsEl, contactEl];
+const navs = [navHomeEl, navProjectsEl, navContactEl];
 
 function pageDeselect(item) {
-    if (item.getAttribute('class') === 'page show') {
+    if (item.getAttribute('class') === 'show') {
         item.setAttribute('class', 'hide');
     }
 }
 
-function pageSelect(event) {
-    event.preventDefault();
-
-    const newLocation = event.target.textContent.toLowerCase();
-
-    if (newLocation != 'home' && newLocation != 'projects' && newLocation != 'contact') {
-        return;
-    }
-    else{
-        pages.forEach(pageDeselect);
-    }
-    
-    if (newLocation === 'home') {
-        homeEl.setAttribute('class', 'page show');
-    }
-    else if (newLocation === 'projects') {
-        projectsEl.setAttribute('class', 'page show');
-    }
-    else if (newLocation === 'contact') {
-        contactEl.setAttribute('class', 'page show');
-    }
-    else {
-        return console.log('all of them')
+function navDeselect(item){
+    if (item.getAttribute('class') === 'selected'){
+        item.setAttribute('class', '');
     }
 }
 
-navEl.addEventListener('click', pageSelect)
+function homeSelect(event) {
+    event.preventDefault();
+
+    pages.forEach(pageDeselect);
+    navs.forEach(navDeselect);
+
+    navHomeEl.setAttribute('class', 'selected');
+    homeEl.setAttribute('class', 'show');
+}
+
+function projectSelect(event) {
+    event.preventDefault();
+
+    pages.forEach(pageDeselect);
+    navs.forEach(navDeselect);
+    
+    navProjectsEl.setAttribute('class', 'selected');
+    projectsEl.setAttribute('class', 'show');
+}
+
+function contactSelect(event) {
+    event.preventDefault();
+
+    pages.forEach(pageDeselect);
+    navs.forEach(navDeselect);
+    
+    navContactEl.setAttribute('class', 'selected');
+    contactEl.setAttribute('class', 'show');
+}
+
+navHomeEl.addEventListener('click', homeSelect);
+navProjectsEl.addEventListener('click', projectSelect);
+navContactEl.addEventListener('click', contactSelect);
