fda97be0d97fc2cdfe80e7a5bc64c94f959dba83
Joshua Rinehart <josh.rinehart.2k@gmail.com>
2024-03-23 20:39:59 -0400

    revamped whole thing

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,56 +1,7 @@
-const navEl = document.querySelector('ol');
-const homeEl = document.getElementById('home');
-const projectsEl = document.getElementById('projects');
-const contactEl = document.getElementById('contact');
-const navHomeEl = document.getElementById('nav-home');
-const navProjectsEl = document.getElementById('nav-projects');
-const navContactEl = document.getElementById('nav-contact');
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar_menu')
 
-const pages = [homeEl, projectsEl, contactEl];
-const navs = [navHomeEl, navProjectsEl, navContactEl];
-
-function pageDeselect(item) {
-    if (item.getAttribute('class') === 'show') {
-        item.setAttribute('class', 'hide');
-    }
-}
-
-function navDeselect(item){
-    if (item.getAttribute('class') === 'selected'){
-        item.setAttribute('class', '');
-    }
-}
-
-function homeSelect(event) {
-    event.preventDefault();
-
-    pages.forEach(pageDeselect);
-    navs.forEach(navDeselect);
-
-    navHomeEl.setAttribute('class', 'selected');
-    homeEl.setAttribute('class', 'show');
-}
-
-function projectSelect(event) {
-    event.preventDefault();
-
-    pages.forEach(pageDeselect);
-    navs.forEach(navDeselect);
-    
-    navProjectsEl.setAttribute('class', 'selected');
-    projectsEl.setAttribute('class', 'show');
-}
-
-function contactSelect(event) {
-    event.preventDefault();
-
-    pages.forEach(pageDeselect);
-    navs.forEach(navDeselect);
-    
-    navContactEl.setAttribute('class', 'selected');
-    contactEl.setAttribute('class', 'show');
-}
-
-navHomeEl.addEventListener('click', homeSelect);
-navProjectsEl.addEventListener('click', projectSelect);
-navContactEl.addEventListener('click', contactSelect);
+menu.addEventListener('click', function () {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+})
